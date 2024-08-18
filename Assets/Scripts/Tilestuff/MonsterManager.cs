6e01d231dfcccdad5545a8f213c4d6792c9e34f6
kinggoldcatter <67169647+kinggoldcatter@users.noreply.github.com>
2024-08-18 13:05:15 -0400

    add new sprites and code

--- Assets/Scripts/Tilestuff/MonsterManager.cs
+++ Assets/Scripts/Tilestuff/MonsterManager.cs
@@ -3,56 +3,105 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(fileName = "MonsterManager", menuName = "MonsterManager")]
 public class MonsterManager : MonoBehaviour
 {
+    [FormerlySerializedAs("Solidmap")] [FormerlySerializedAs("map")] [SerializeField]
+    private Tilemap solidmap;
     [SerializeField]
-    private Tilemap map;
+    private Tilemap softmap;
+    
+    // Bed Monster stages
+    [FormerlySerializedAs("firstStage")] [SerializeField]
+    private TileBase firstStage1;
     [SerializeField]
-    private TileBase firstStage;
+    private TileBase firstStage2;
+    [FormerlySerializedAs("secondStage")] [SerializeField]
+    private TileBase secondStage1;
     [SerializeField]
-    private TileBase secondStage;
+    private TileBase secondStage2;
+    [FormerlySerializedAs("finalStage")] [SerializeField]
+    private TileBase finalStage1;
     [SerializeField]
-    private TileBase finalStage;
+    private TileBase finalStage2;
+    
+    [FormerlySerializedAs("bedmonsterPosition")] [SerializeField]
+    private Vector3Int bedmonsterPosition1 = new Vector3Int(0, 2, 0);
     [SerializeField]
-    private Vector3Int bedmonsterPosition = new Vector3Int(0, 2, 0);
+    private Vector3Int bedmonsterPosition2 = new Vector3Int(0, 0, 0);
 
     [SerializeField] private float angerInterval = 20;
     
-    [SerializeField]
-    private float angerTimer = 20;
-    
+    [FormerlySerializedAs("angerTimer")] [SerializeField]
+    private float bedMonsterAngerTimer = 20;
     
     [SerializeField]
     private byte bedMonsterAnger = 1;
     
+    //Window monster stuff
+    [SerializeField]
+    private TileBase windowFirstStage1;
+    [SerializeField]
+    private TileBase windowFirstStage2;
+    [SerializeField]
+    private TileBase windowSecondStage1;
+    [SerializeField]
+    private TileBase windowSecondStage2;
+    [SerializeField]
+    private TileBase windowFinalStage1;
+    [SerializeField]
+    private TileBase windowFinalStage2;
+    
+    [SerializeField]
+    private Vector3Int windowMonsterPosition1 = new Vector3Int(0, 2, 0);
+    [SerializeField]
+    private Vector3Int windowMonsterPosition2 = new Vector3Int(0, 0, 0);
+
+    [SerializeField]
+    private float windowMonsterAngerTimer = 20;
+    [SerializeField]
+    private float windowAngerInterval = 20;
+
+    [SerializeField]
+    private byte windowMonsterAnger = 1;
+    
+    //update tiles
     
     internal void Update()
     {
-        angerTimer -= Time.deltaTime;
-        if (angerTimer <= 0)
+        bedMonsterAngerTimer -= Time.deltaTime;
+        windowMonsterAngerTimer -= Time.deltaTime;
+        if (bedMonsterAngerTimer <= 0)
         {
-            increaseAngerStage();
-            angerTimer = angerInterval;
+            BedIncreaseAngerStage();
+            bedMonsterAngerTimer = angerInterval;
         }
+
+        if (!(windowMonsterAngerTimer <= 0)) return;
+        WindowIncreaseAngerStage();
+        windowMonsterAngerTimer = windowAngerInterval;
     }
 
-    public void increaseAngerStage()
+    private void BedIncreaseAngerStage()
     {
         switch (bedMonsterAnger)
         {
-            case 0 : map.SetTile(bedmonsterPosition, firstStage);
+            case 0 : solidmap.SetTile(bedmonsterPosition1, firstStage1);
+                solidmap.SetTile(bedmonsterPosition2, firstStage2);
                 bedMonsterAnger++;
                 break;
-            case 1 : map.SetTile(bedmonsterPosition, firstStage); 
+            case 1 : solidmap.SetTile(bedmonsterPosition1, firstStage1); 
+                solidmap.SetTile(bedmonsterPosition2, firstStage2);
                 bedMonsterAnger++;
                 break;
-            case 2 : map.SetTile(bedmonsterPosition, secondStage);
+            case 2 : solidmap.SetTile(bedmonsterPosition1, secondStage1);
+                solidmap.SetTile(bedmonsterPosition2, secondStage2);
                 bedMonsterAnger++;
                 break;
-            case 3 : map.SetTile(bedmonsterPosition, finalStage);
+            case 3 : solidmap.SetTile(bedmonsterPosition1, finalStage1);
+                solidmap.SetTile(bedmonsterPosition2, finalStage2);
                 break;
             default:
                 Debug.Log("Error");
@@ -61,11 +110,38 @@ public class MonsterManager : MonoBehaviour
             
         }
     }
+    private void WindowIncreaseAngerStage()
+    {
+        switch (windowMonsterAnger)
+        {
+            case 0 : solidmap.SetTile(windowMonsterPosition1, windowFirstStage1);
+                softmap.SetTile(windowMonsterPosition2, windowFirstStage2);
+                bedMonsterAnger++;
+                break;
+            case 1 : solidmap.SetTile(windowMonsterPosition1, windowFirstStage1);
+                softmap.SetTile(windowMonsterPosition2, windowFirstStage2);
+                bedMonsterAnger++;
+                break;
+            case 2 : solidmap.SetTile(windowMonsterPosition1, secondStage1);
+                softmap.SetTile(windowMonsterPosition2, secondStage2);
+                bedMonsterAnger++;
+                break;
+            case 3 : solidmap.SetTile(windowMonsterPosition1, finalStage1);
+                softmap.SetTile(windowMonsterPosition2, finalStage2);
+                break;
+            default:
+                Debug.Log("Error");
+                windowMonsterAnger = 0;
+                throw new ArgumentException("Invalid anger stage");
+            
+        }
+    }
 
     public void BanishMonster()
     {
         bedMonsterAnger = 0;
-        map.SetTile(bedmonsterPosition, firstStage);
+        solidmap.SetTile(bedmonsterPosition1, firstStage1);
+        solidmap.SetTile(bedmonsterPosition2, firstStage2);
         Debug.Log("Banish monster");
     }
 }
